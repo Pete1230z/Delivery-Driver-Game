f141015f59ddd415826bd5920e3b4265f81d2905
Nicholas Petree <petree234@gmail.com>
2024-09-24 15:38:09 -0500

    Added SerializedField to variables so they are visible in inspector.

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    float steerSpeed = 1f;
-    float moveSpeed = 0.01f;
+    //SerializedField is to make the variable visible in the inspector
+    [SerializeField]float steerSpeed = 1f;
+    [SerializeField]float moveSpeed = 0.01f;
 
     // Start is called before the first frame update
     void Start()
