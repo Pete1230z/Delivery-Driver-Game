cb9616be881fb6d8ef3004c1847ce12778fea5a0
Nicholas Petree <petree234@gmail.com>
2024-09-25 05:48:38 -0500

    Created variables for when car has and does not have a package.

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    [SerializeField] Color32 hasPackageColor = new Color32(255, 0, 0, 255);
+    [SerializeField] Color32 noPackageColor = new Color32(0, 255, 0, 255);
     bool hasPackage = false;
+    [SerializeField] float destroySpeed = 0.5f;
     private void OnCollisionEnter2D(Collision2D other) {
         Debug.Log("Macy be careful!");
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
-        if (other.tag == "Package") {
+        //If player has a package they can not pick up another one
+        if (other.tag == "Package" && !hasPackage) {
             Debug.Log("Package picked up!");
             hasPackage = true;
+            Destroy(other.gameObject, destroySpeed);
         } else if (other.tag == "Customer" && hasPackage) {
             Debug.Log("Package delivered!");
             //Once the package is delivered, the customer can not drop it off again
