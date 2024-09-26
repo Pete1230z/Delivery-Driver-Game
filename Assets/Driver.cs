cfff2a7eb156831fdb21d483c116ac62b1d168dd
Nicholas Petree <petree234@gmail.com>
2024-09-25 19:44:24 -0500

    Added methods to boost and slow down.

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -6,7 +6,9 @@ public class Driver : MonoBehaviour
 {
     //SerializedField is to make the variable visible in the inspector
     [SerializeField]float steerSpeed = 1f;
-    [SerializeField]float moveSpeed = 0.01f;
+    [SerializeField]float moveSpeed = 20f;
+    [SerializeField]float slowSpeed = 15f;
+    [SerializeField]float boostSpeed = 30f;
 
     // Start is called before the first frame update
     void Start()
@@ -30,5 +32,15 @@ public class Driver : MonoBehaviour
         //f is to denote that is a float
         transform.Rotate(0, 0, -steerAmount);
         transform.Translate(0, moveAmount, 0);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        moveSpeed = slowSpeed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+            if(other.tag == "Boost") {
+                moveSpeed = boostSpeed;
+            }
     }
 }
